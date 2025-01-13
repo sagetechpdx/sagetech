9f1d32375343889e41ba74a8576ce92470f14953
sagetechpdx <info@sagetech.support>
2025-01-12 23:59:23 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://positivustheme.vercel.app",
+  site: "https://sagetech.netlify.app",
   integrations: [tailwind()],
   output: 'server',
   adapter: vercel(),
