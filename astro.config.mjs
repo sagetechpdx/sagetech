f5fc0047ac9dbc73b849b7917fad04f87964c0f5
sagetechpdx <info@sagetech.support>
2025-01-13 00:29:09 -0800

    o

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
 // Import the Vercel adapter
-import vercel from "@astrojs/vercel";
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sagetech.vercel.app",
   integrations: [tailwind()],
-  output: "server",
+  output: 'server',
   adapter: vercel(),
-  alias: {
-    "@components": "./src/components",
-    "@layouts": "./src/layouts",
-    "@styles": "./src/styles"
-  }
 });
